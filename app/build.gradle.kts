80d515fc052eafc31a3d84fe6218299f6bc66642
Fr1sby <1975.serch.1975@gmail.com>
2026-03-20 13:25:44 +0300

    Day 5: Refactored to MVVM architecture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,9 @@ android {
     namespace = "ru.hramova.myfirstapp"
     compileSdk = 36
 
+    buildFeatures {
+        viewBinding = true
+    }
     defaultConfig {
         applicationId = "ru.hramova.myfirstapp"
         minSdk = 24
@@ -29,32 +32,33 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
 
     }
-    kotlinOptions {
-        jvmTarget = "11"
-        jvmTarget = "1.8"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
+
     }
 
-    buildFeatures {
-        viewBinding = true
+    dependencies {
+        // Lifecycle и ViewModel
+        //noinspection UseTomlInstead
+        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
+        // LiveData
+        //noinspection UseTomlInstead
+        implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
+        // Для viewModels delegation
+        //noinspection UseTomlInstead
+        implementation("androidx.activity:activity-ktx:1.13.0")
+
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.appcompat)
+        implementation(libs.material)
+        implementation(libs.androidx.activity)
+        implementation(libs.androidx.constraintlayout)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
     }
-
-}
-
-dependencies {
-    //noinspection UseTomlInstead
-    implementation("androidx.activity:activity-ktx:1.13.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
